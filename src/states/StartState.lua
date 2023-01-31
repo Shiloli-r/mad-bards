66cafb1b3bda08262fa1ef807f74dd8ee317c361
Shiloli-r <ronaldshiloli@gmail.com>
2023-01-31 11:00:02 +0300

    ground, walls and aliens

--- src/states/StartState.lua
+++ src/states/StartState.lua
@@ -4,7 +4,24 @@ function StartState:init()
     self.background = Background()
     self.world = love.physics.newWorld(0, 300)
 
+    -- ground
+    self.groundBody = love.physics.newBody(self.world, 0, VIRTUAL_HEIGHT, 'static')
+    self.groundShape = love.physics.newEdgeShape(0, 0, VIRTUAL_WIDTH, 0)
+    self.groundFixture = love.physics.newFixture(self.groundBody, self.groundShape)
 
+    -- walls
+    self.leftWallBody = love.physics.newBody(self.world, 0, 0, 'static')
+    self.rightWallBody = love.physics.newBody(self.world, VIRTUAL_WIDTH, 0, 'static')
+    self.wallShape = love.physics.newEdgeShape(0, 0, 0, VIRTUAL_HEIGHT)
+    self.leftWallFixture = love.physics.newFixture(self.leftWallBody, self.wallShape)
+    self.rightWallFixture = love.physics.newFixture(self.rightWallBody, self.wallShape)
+
+    -- lots of aliens
+    self.aliens = {}
+
+    for i = 1, 100 do
+        table.insert(self.aliens, Alien(self.world))
+    end    
 end
 
 function StartState:update(dt)
@@ -21,6 +38,10 @@ end
 
 function StartState:render()
     self.background:render()
+
+    for k, alien in pairs(self.aliens) do
+        alien:render()
+    end    
 
     -- title text
     love.graphics.setColor(64/255, 64/255, 64/255, 200/255)
